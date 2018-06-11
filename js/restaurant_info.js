91b4ab3d5fe2d743b284b08677f5a2afde74ef10
Tina <tmorozco1990@gmail.com>
2018-06-11 10:57:08 -0600

    refactor: adding alt attributes to the page images
    * adding alt attr to the imgs in the restaurant section and in the main.js section as well

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'Restaurant Image'
+
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
