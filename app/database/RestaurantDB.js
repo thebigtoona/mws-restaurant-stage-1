e4404360fd2e3ec1f76c4a6ecb0d12e5a03b073f
Tina <tmorozco1990@gmail.com>
2018-07-17 13:30:08 -0600

    refactor: adding return statement to opendatabase funciton

--- app/database/RestaurantDB.js
+++ app/database/RestaurantDB.js
@@ -1,11 +1,14 @@
 import idb from 'idb';
+
 class RestaurantDB {
   static openDatabase() {
-    idb.open('mws-restaurant-reviews', 1, upgradeDB => {
+    return idb.open('mws-restaurant-reviews', 1, upgradeDB => {
       switch (upgradeDB.oldVersion) {
         case 0: upgradeDB.createObjectStore('restaurant-data', {keyPath: 'id'})
       }
     })
   }
 }
+
+
 export { RestaurantDB as default }
