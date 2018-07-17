bf89873e4cc7fdcee531b36722bf7eee1e0eae71
Tina <tmorozco1990@gmail.com>
2018-07-17 13:20:16 -0600

    refactor: implementing db name and object store

--- app/database/RestaurantDB.js
+++ app/database/RestaurantDB.js
@@ -1,8 +1,10 @@
 import idb from 'idb';
 class RestaurantDB {
   static openDatabase() {
-    idb.open('test', 1, upgradeDB => {
-      console.log('db is open')
+    idb.open('mws-restaurant-reviews', 1, upgradeDB => {
+      switch (upgradeDB.oldVersion) {
+        case 0: upgradeDB.createObjectStore('restaurant-data', {keyPath: 'id'})
+      }
     })
   }
 }
